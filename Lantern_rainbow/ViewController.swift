5f50fd20e5ef9286da9312fbdb6617ed4957b6c3
MacBookAir <macbookair@mila.dlink>
2022-04-16 22:42:27 +0300

    Initial Commit

---
+++ Lantern_rainbow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lantern_rainbow
+//
+//  Created by MacBookAir on 16.04.2022.
+//  Copyright © 2022 MacBookAir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
